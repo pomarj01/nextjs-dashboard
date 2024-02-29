f3a1eb4f2932b9ac21a422b6c53d6e1aec1e8d86
Jill <pomarj01@gmail.com>
2024-02-29 15:09:18 +0000

    Use useFormStatus to handle pending state of the form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,7 +8,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { useFormState } from 'react-dom';
+import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '../lib/actions';
 
 export default function LoginForm() {
@@ -83,8 +83,10 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
+
   return (
-    <Button className="mt-4 w-full">
+    <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
